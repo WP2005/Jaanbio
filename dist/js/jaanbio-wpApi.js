4f203eb3ef60b9c8560a90cccdf2119e29014db8
WP2005 <jylee0128@gmail.com>
2022-10-28 10:19:01 -0500

    modified:   dist/index.html modified:   dist/js/jaanbio-wpApi.js

--- dist/js/jaanbio-wpApi.js
+++ dist/js/jaanbio-wpApi.js
@@ -8,9 +8,25 @@ function extractPostData(data) {
 
             postArray.push([postId, category, title, content]);
         };
-        console.table(postArray)
-     
+        console.table(postArray);
   }
+
+  function createPostObj(data) {
+    var postObj=[];
+    for(var i in data){
+        let postId=data[i].id
+        let title=data[i].title.rendered
+        let category=data[i].categories[0];
+        let content=data[i].content.rendered;
+
+        postObj.push({"postId":postId, "title":title, "category":category, "content":content})
+      
+    };
+    console.log("Obj type: ", typeof(postObj))
+    console.table('postobj is:', postObj);
+    console.log('element is:', postObj[0]['postId']);
+}
+
   
   
 
@@ -25,7 +41,7 @@ const get_wpPosts = function (postURL) {
     })
     .then((data) => {
         
-      extractPostData(data); 
+      createPostObj(data); 
     })
     .catch((error) => {
       console.log("Error in fetch: ", error);
@@ -36,5 +52,5 @@ const get_wpPosts = function (postURL) {
 //* _embed to include the featured image source. NOTE THE UNDERSCORE */
 const postURL =
   "https://jaanbio.com/wp-json/wp/v2/posts";
-get_wpPosts(postURL);
+  get_wpPosts(postURL);
 
